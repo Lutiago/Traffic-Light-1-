67ac1178d8f56de8bfc56e85189028c67a535e06
Lutiago <perillaluisa11@gmail.com>
2023-03-09 01:18:37 +0000

    trafficlight

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import { Footer } from "@/components/Footer";
+import { TrafficLight } from "@/components/TrafficLight";
 import { Welcome } from "@/components/Welcome";
 
 export default function Home() {
   return (
     <div className="container">
-      <Welcome />
+      <TrafficLight />
       <Footer />
     </div>
   );
